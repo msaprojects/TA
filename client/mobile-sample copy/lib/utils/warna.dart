213e3e0c874330b2adc6631a657d9dd779c50aab
msaprojects <msadev96@gmail.com>
2022-08-12 09:38:17 +0700

    delete mobile fc

--- client/mobile-sample copy/lib/utils/warna.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const backgroundcolor = Color.fromRGBO(255, 255, 255, 1);
-const yellowcolor = Color.fromRGBO(255, 211, 11, 1);
-const blackcolor = Color.fromRGBO(41, 55, 80, 1);
-const green = Color.fromRGBO(0, 177, 195, 1);
-const greenold = Color.fromRGBO(0, 152, 171, 1);
